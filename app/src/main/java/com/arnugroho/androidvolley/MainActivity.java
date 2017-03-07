98d79a06cbe442c08c4c86a6894a0022b58d7f33
akbar riyan nugroho - arnugroho <nugroho.akbar.r@gmail.com>
2017-03-07 11:28:45 +0700

    add volley Json Arrays

--- app/src/main/java/com/arnugroho/androidvolley/MainActivity.java
+++ app/src/main/java/com/arnugroho/androidvolley/MainActivity.java
@@ -15,14 +15,16 @@ import android.widget.TextView;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String IMAGE_REQUEST_URL = "http://androidtutorialpoint.com/api/lg_nexus_5x";
+    private static final String IMAGE_REQUEST_URL = "https://androidtutorialpoint.com/api/lg_nexus_5x";
     private static final String STRING_REQUEST_URL = "https://androidtutorialpoint.com/api/volleyString";
     private static final String JSON_OBJECT_REQUEST_URL = "https://androidtutorialpoint.com/api/volleyJsonObject";
     private static final String JSON_ARRAY_REQUEST_URL = "https://androidtutorialpoint.com/api/volleyJsonArray";
@@ -46,8 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
         stringRequestButton = (Button)findViewById(R.id.button_get_string);
         jsonObjectRequestButton = (Button)findViewById(R.id.button_get_Json_object);
-        /*JsonArrayRequestButton = (Button)findViewById(R.id.button_get_Json_array);
-        ImageRequestButton = (Button)findViewById(R.id.button_get_image);*/
+        jsonArrayRequestButton = (Button)findViewById(R.id.button_get_Json_array);
+        /*ImageRequestButton = (Button)findViewById(R.id.button_get_image);*/
 
         stringRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +62,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 volleyJsonObjectRequest(JSON_OBJECT_REQUEST_URL);
+            }
+        });
+
+        jsonArrayRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                volleyJsonArrayRequest(JSON_ARRAY_REQUEST_URL);
             }
         });
 
@@ -142,5 +151,43 @@ public class MainActivity extends AppCompatActivity {
 
         // Adding JsonObject request to request queue
         AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectReq,REQUEST_TAG);
+    }
+
+    public void volleyJsonArrayRequest(String url){
+
+        String  REQUEST_TAG = "com.androidtutorialpoint.volleyJsonArrayRequest";
+        progressDialog.setMessage("Loading...");
+        progressDialog.show();
+
+        JsonArrayRequest jsonArrayReq = new JsonArrayRequest(url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+                        LayoutInflater li = LayoutInflater.from(MainActivity.this);
+                        showDialogView = li.inflate(R.layout.show_dialog, null);
+                        outputTextView = (TextView)showDialogView.findViewById(R.id.text_view_dialog);
+                        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                        alertDialogBuilder.setView(showDialogView);
+                        alertDialogBuilder
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                    }
+                                })
+                                .setCancelable(false)
+                                .create();
+                        outputTextView.setText(response.toString());
+                        alertDialogBuilder.show();
+                        progressDialog.hide();                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                progressDialog.hide();
+            }
+        });
+
+        // Adding JsonObject request to request queue
+        AppSingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrayReq, REQUEST_TAG);
     }
 }
